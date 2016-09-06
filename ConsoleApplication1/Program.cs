1727d58531da68d8fd1d4ceb44194ea407d93cbe
unknown <>
2016-09-06 16:26:52 +0800

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static ushort[] irData;
+        static byte[] irDataConvert;
+        static void Main(string[] args)
+        {
+            KinectSensor sensor;
+            InfraredFrameReader irReader;
+            sensor = KinectSensor.GetDefault();
+            irReader = sensor.InfraredFrameSource.OpenReader();
+            FrameDescription fd = sensor.InfraredFrameSource.FrameDescription;
+            irData = new ushort[fd.LengthInPixels];
+            irDataConvert = new byte[irData.Length * 4];
+            Console.ForegroundColor = ConsoleColor.Green;
+            sensor.Open();
+            irReader.FrameArrived += IrReader_FrameArrived;
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar)
+            {
+
+            }
+        }
+        private static void IrReader_FrameArrived(object sender, InfraredFrameArrivedEventArgs e)
+        {
+            // ColorFrame is IDisposable
+            using (InfraredFrame irFrame = e.FrameReference.AcquireFrame())
+            {
+                if (irFrame != null)
+                {
+                    irFrame.CopyFrameDataToArray(irData);
+                    foreach (var data in irData)
+                        Console.Write(data);
+                }
+            }
+        }
+    }
+}
